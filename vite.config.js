081836402f31b9499b79bd12414cbda7d6b60529
MAW PAING THU <paingmawthu@gmail.com>
2026-06-09 21:46:20 +0900

    Site အသစ် ပြန်ပြင်သည်

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import viteImagemin from 'vite-plugin-imagemin';
 import { resolve } from 'path';
 
 export default defineConfig({
@@ -23,4 +24,12 @@ export default defineConfig({
             },
         },
     },
+    plugins: [
+        viteImagemin({
+        gifsicle: { optimizationLevel: 7, interlaced: false },
+        optipng: { optimizationLevel: 7 },
+        mozjpeg: { quality: 20 },
+        pngquant: { quality: [0.8, 0.9], speed: 4 },
+        }),
+    ],
 });
